ab6e078a21ea777b1f8c089da779237af19124b5
Moaad <moaadagdali2005@gmail.com>
2025-02-27 13:47:23 +0100

    commit

--- index.php
+++ index.php
@@ -43,7 +43,7 @@ if (!$data || !isset($data["title"], $data["poster_url"], $data["release_date"],
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
 
     <section>
-        <h1 class="text-center mx-auto p-2">¿Cuál es la siguiente película de Marvel?</h1>
+        <h1 class="text-center mx-auto p-2">La siguiente película de Marvel que se estrenará es:</h1>
         <div class="text-center">
             <img src="<?= htmlspecialchars($data["poster_url"]) ?>" class="rounded img-fluid" width="400" 
                 alt="Cartel de estreno de <?= htmlspecialchars($data["title"]) ?>">
@@ -53,7 +53,7 @@ if (!$data || !isset($data["title"], $data["poster_url"], $data["release_date"],
     <hgroup class="text-center">
         <h2 class="mx-auto p-2"><?= htmlspecialchars($data["title"]) ?> se estrena en <?= (int)$data["days_until"] ?> días</h2>
         <p class="text-center lead h6">Fecha de estreno: <?= htmlspecialchars($data["release_date"]) ?></p>
-        <p class="text-center lead h6">La siguiente película es: <?= htmlspecialchars($data["following_production"]["title"]) ?></p>
+        <p class="text-center lead h6">La siguiente película será: <?= htmlspecialchars($data["following_production"]["title"]) ?></p>
     </hgroup>
 
 </body>
